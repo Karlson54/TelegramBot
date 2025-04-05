d25872bfc30dfd3932b4c25e1708873ac72a35a3
“Karlson54” <anruban2001@gmail.com>
2025-04-06 02:54:29 +0300

    bug fixes

--- app.py
+++ app.py
@@ -13,17 +13,34 @@ logger = setup_logger()
 state_storage = StateMemoryStorage()
 bot = telebot.TeleBot(BotConfig.TOKEN, state_storage=state_storage)
 
+# Удаляем вебхук перед запуском long polling
+bot.remove_webhook()
+
+# Импортируем модели перед созданием таблиц
+from database.models import User, Product, Order, OrderItem, Payment, Feedback
+
 # Создание таблиц в БД, если их нет
 db_manager.create_tables()
 
 # Импорт обработчиков
-# from handlers import admin_handlers, catalog_handlers, common_handlers, order_handlers, payment_handlers
+from handlers import admin_handlers, catalog_handlers, common_handlers, order_handlers, payment_handlers
 
 if __name__ == '__main__':
     logger.info("Запуск бота магазина...")
     
     # Регистрация фильтров
     bot.add_custom_filter(custom_filters.StateFilter(bot))
+    
+    # Регистрация обработчиков
+    common_handlers.register_common_handlers(bot)
+    catalog_handlers.register_catalog_handlers(bot)
+    admin_handlers.register_admin_handlers(bot)
+    
+    # Добавьте регистрацию остальных обработчиков, когда они будут готовы
+    # order_handlers.register_order_handlers(bot)
+    # payment_handlers.register_payment_handlers(bot)
+    
+    logger.info("Бот запущен...")
     
     # Запуск бота
     bot.infinity_polling(skip_pending=True)
--- config/logging_config.py
+++ config/logging_config.py
@@ -8,23 +8,26 @@ def setup_logger():
         os.makedirs('logs')
         
     logger = logging.getLogger('bot_logger')
-    logger.setLevel(logging.INFO)
     
-    # Лог в файл
-    file_handler = RotatingFileHandler(
-        'logs/bot.log', 
-        maxBytes=1024*1024*5,  # 5 MB
-        backupCount=3
-    )
-    file_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    file_handler.setFormatter(file_format)
-    
-    # Лог в консоль
-    console_handler = logging.StreamHandler()
-    console_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    console_handler.setFormatter(console_format)
-    
-    logger.addHandler(file_handler)
-    logger.addHandler(console_handler)
+    # Проверяем, были ли уже добавлены обработчики к логгеру
+    if not logger.handlers:
+        logger.setLevel(logging.INFO)
+        
+        # Лог в файл
+        file_handler = RotatingFileHandler(
+            'logs/bot.log', 
+            maxBytes=1024*1024*5,  # 5 MB
+            backupCount=3
+        )
+        file_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        file_handler.setFormatter(file_format)
+        
+        # Лог в консоль
+        console_handler = logging.StreamHandler()
+        console_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        console_handler.setFormatter(console_format)
+        
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
     
     return logger
